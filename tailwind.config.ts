4792e73ac879d1fb36aa48cb04b596efee6624f7
ShakilOfficial <mrshakilhossainofficial@gmail.com>
2024-11-09 15:58:29 +0600

    initial setup done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        DEFAULT: "#000",
+        100: "#000319",
       },
     },
   },
